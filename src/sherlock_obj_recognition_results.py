2f1b7dcaa9f4836e69192ff1d7fc05e54c08148f
minana96 <60661838+minana96@users.noreply.github.com>
2021-06-11 15:25:37 +0200

    loging object recognition result msgs

--- src/sherlock_obj_recognition_results.py
+++ src/sherlock_obj_recognition_results.py
@@ -1,40 +1,25 @@
 #!/usr/bin/env python  
 
 import rospy
-from std_msgs.msg import Float32MultiArray, MultiArrayLayout
-
+from std_msgs.msg import Float32MultiArray
 
 class SherlockObjRecognition:
 
     def __init__(self):
         self.objects_subscriber = rospy.Subscriber("/objects", Float32MultiArray, self.callback_objects)
-        self.previously_recognised = set()
-        self.object_offset = 12
-        self.object_ids = rospy.get_param('~object_ids', {})
-
 
     def callback_objects(self, msg):
         recognised_objects = msg.data
-        currently_recognised = set(recognised_objects[::self.object_offset])
-
-        # If recognised objects in this frame are different than ones in the previous frame
-        if not ((self.previously_recognised <= currently_recognised) and (currently_recognised <= self.previously_recognised)):
-            self.print_set(currently_recognised)
-
-        self.previously_recognised = currently_recognised
-
+        
+        self.currently_recognised = set(recognised_objects[::12]) 
+        self.print_object_ids()
     
-    def print_set(self, set_of_object_ids):
-        if len(set_of_object_ids) == 0:
+    def print_object_ids(self):
+        if len(self.currently_recognised) == 0:
             rospy.loginfo("No object recognised")
-            return
-
-        try:
-            object_output = ", ".join([self.object_ids[str(int(obj_id))] for obj_id in set_of_object_ids])
-            rospy.loginfo(object_output + " recognised")
-        except KeyError:
-            rospy.logerr("Invalid key!")
-
+        else:
+            object_output = ", ".join([f"Object {int(obj_id)}" for obj_id in self.currently_recognised])
+            rospy.loginfo(object_output + " detected!")
 
 
 if __name__ == '__main__':
